b0cc77f97fba86ebabb80e9f41293630eb861622
Amamul Haq <mdamamul78692@gmail.com>
2025-01-27 21:07:31 +0545

    linked list for deletion from and to

---
+++ c++/linkedlist2.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void display(Node* &head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << "Data: " << temp->data << " ";
+        cout << "Pointing: " << temp << endl;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void InsertAtHead(Node* &head, int d) {
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void InsertAtTail(Node* &tail, int d) {
+    Node* temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void deleteFromTo(Node* &head, int a, int b) {
+    Node* from = head;
+    Node* prev = NULL;
+
+    for (int i = 1; from != NULL && i < a; i++) {
+        prev = from;
+        from = from->next;
+    }
+
+    for (int i = a; from != NULL && i <= b; i++) {
+        Node* next = from->next;
+        delete from;
+        from = next;
+    }
+
+    if (prev != NULL) {
+        prev->next = from;
+    } else {
+        head = from;
+    }
+}
+
+int main() {
+    Node* link1 = new Node(10);
+    Node* head = link1;
+    Node* tail = link1;
+
+    Node* link2 = new Node(30);
+    link1->next = link2;
+    tail = link2;
+
+    Node* link3 = new Node(40);
+    link2->next = link3;
+    tail = link3;
+
+    InsertAtHead(head, 12);
+    display(head);
+
+    InsertAtTail(tail, 43);
+    display(head);
+
+    deleteFromTo(head, 2, 3);
+    display(head);
+
+    return 0;
+}
