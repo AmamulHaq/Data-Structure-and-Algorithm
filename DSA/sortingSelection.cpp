7c28ae371fd0426317f95630a6f2a6a8e1512acf
Amamul Haq <mdamamul78692@gmail.com>
2025-04-11 21:58:03 +0545

    selection sort

--- DSA/sortingSelection.cpp
+++ DSA/sortingSelection.cpp
@@ -2,17 +2,9 @@
 using namespace std;
 
 int main() {
-    int SIZE;
-    cout << "Enter no of elements: ";
-    cin >> SIZE;
-    int array[SIZE];
-
-    cout << "Enter elements: " << endl;
-    for (int i = 0; i < SIZE; i++) {
-        cin >> array[i];
-    }
-    //int array[SIZE]={25,57,48,37,12,92,86,33};
-// selection sort for min index
+    int SIZE = 8;
+    int array[8] ={25,57,48,37,12,92,86,33};
+/* selection sort for min index
     for (int i = 0; i < SIZE - 1; i++) {
         int min = i;
         for (int j = i + 1; j < SIZE; j++) {
@@ -24,7 +16,18 @@ int main() {
             swap(array[i],array[min]);
         }
     }
-//
+*/
+    // Selection sort for max index
+    for (int i = SIZE-1; i >= 0; i--) {
+        int max = 0;
+        for (int j = 1; j <=i ; j++) {
+            if (array[j] > array[max]) {
+                max = j;
+            }
+        }
+        swap(array[max], array[i]);
+    }
+
     cout << "Sorted array: ";
     for (int i = 0; i < SIZE; i++) {
         cout << array[i] << " ";
