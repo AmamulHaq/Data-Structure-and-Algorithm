8df1dd5946d04e3b2ebf1b049c0c1f98ddfbdf21
Amamul Haq <mdamamul78692@gmail.com>
2025-04-11 20:55:19 +0545

    insertion sort

--- DSA/sortingInsertion.cpp
+++ DSA/sortingInsertion.cpp
@@ -13,12 +13,11 @@ int main() {
     //int array[SIZE]={25,57,48,37,12,92,86,33};
     //insertion algorithm
     for (int i = 1; i < SIZE; i++) {
-        if (array[i - 1] > array[i]) {
-            for (int j = i; j > 0 && array[j - 1] > array[j]; j--) {
-            swap(array[j - 1] , array[j]);
-            }
+        for (int j = i; j > 0 && array[j-1] > array[j]; j--) {
+            swap(array[j-1], array[j]);
         }
     }
+    
     //
     for (int i = 0; i < SIZE; i++) {
         cout << array[i] << " ";
